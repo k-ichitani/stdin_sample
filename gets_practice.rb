45da6136537f793144fdfae68cf47d038a4ac68c
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-08-09 04:08:36 +0000

    [update]p,puts,gets,readlines,split,each

--- gets_practice.rb
+++ gets_practice.rb
@@ -1,2 +1,7 @@
+# line = gets.split(' ')
+# p line
+
 line = gets.split(' ')
-p line
+line.each do |word|
+  p word
+end
--- readlines_practice.rb
+++ readlines_practice.rb
@@ -1,5 +1,17 @@
+# lines = readlines
+# lines.each do |words|
+#   p words.chomp.split(' ')
+# end
+
+# lines = readlines
+# lines.each do |words|
+#   p words.split # 改行文字も分割するために使用されるため、chompは不要
+# end
+
+# 全ての配列の全ての要素を一つずつ標準出力する
 lines = readlines
 lines.each do |words|
-  p words.chomp.split(' ')
+  words.chomp.split(' ').each do |word|
+    p word
+  end
 end
-
